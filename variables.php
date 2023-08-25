e3c3ac1e81a010f3972a476ae9fa61b87c50fa8b
Igor Santos <igorsantos9513@outlook.com>
2023-08-24 22:25:31 -0300

    <3

--- variables.php
+++ variables.php
@@ -1,10 +1,10 @@
 <?php
 //Mostrando o case sensitive
-    // $He4rt = 'Eu ';
-    // $he4rt = 'amo ';
-    // $he4rT = 'a He4rt ';
-    // $HE4RT = 'Developers';
-    // echo $He4rt . $he4rt . $he4rT . $HE4RT;
+    $He4rt = 'Eu ';
+    $he4rt = 'amo ';
+    $he4rT = 'a He4rt ';
+    $HE4RT = 'Developers';
+    echo $He4rt . $he4rt . $he4rT . $HE4RT;
 
     $reais = 1;
 
